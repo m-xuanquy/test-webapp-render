05da0027bbe6640b52e6ffe420df24357a8a8f26
mxquy <22120303@student.hcmus.edu.vn>
2024-09-22 14:33:05 +0700

    commit 4

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 5500
 // Static Files
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/img', express.static(__dirname + 'public/img'))
-app.use('/js', express.static(__dirname + 'public/js'))
+
 
 // Templating Engine
 app.set('views', './src/views')
