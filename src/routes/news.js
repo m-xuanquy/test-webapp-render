93760b84f87cc1aa905f57e61df265d7667a6659
mxquy <22120303@student.hcmus.edu.vn>
2024-09-23 12:20:04 +0700

    commit

--- src/routes/news.js
+++ src/routes/news.js
@@ -5,18 +5,18 @@ const axios = require('axios')
 newsRouter.get('', async (req, res) => {
     try {
         const newsAPI = await axios.get(`https://raddy.dev/wp-json/wp/v2/posts/`)
-        res.render('index', { articles: newsAPI.data })
+        res.render('index.ejs', { articles: newsAPI.data })
     } catch (err) {
         if (err.response) {
-            res.render('index', { articles: null })
+            res.render('index.ejs', { articles: null })
             console.log(err.response.data)
             console.log(err.response.status)
             console.log(err.response.headers)
         } else if (err.request) {
-            res.render('index', { articles: null })
+            res.render('index.ejs', { articles: null })
             console.log(err.requiest)
         } else {
-            res.render('index', { articles: null })
+            res.render('index.ejs', { articles: null })
             console.error('Error', err.message)
         }
     }
